3eb33843e788afc425c38c0b97e0a3ad41803c22
David Billskog <billskog@gmail.com>
2013-06-23 20:49:37 +0200

    Calculate basic growth

---
+++ lib/portfolio.rb
@@ -0,0 +1,44 @@
+class Portfolio
+  attr_reader :growth, :date, :stocks, :prices
+
+  def initialize date=nil, stocks={}, growth=1, prices={}
+    @date = date
+    @stocks = stocks
+    @growth = growth
+    @prices = prices
+  end
+
+  def transaction date, ticker, quantity, price
+    return self.class.new date, { ticker => quantity }, 1, { ticker => price } unless self.date
+
+    period_growth = self.value(date: date, prices: {ticker => price}) / self.value
+    stocks_after_transaction = stocks.merge({ticker => quantity}) {|key, before, added| before + added }
+
+    self.class.new date, stocks_after_transaction, growth * period_growth, { ticker => price }
+  end
+
+  # Calculates the value of the enture portfolio.
+  #
+  # options - Used to redefine how value is calculated:
+  #           :prices - a Hash of ticker plus price to override fetched prices from omx.
+  #           :date   - calculate the value of a portfolio on a specific date.
+  #
+  # Returns nothing.
+  def value(options={})
+    actual_prices = options.fetch(:prices, {})
+    actual_date = options.fetch(:date, date)
+    value = 0
+    @stocks.each do |ticker, quantity|
+      price = actual_prices[ticker] || prices[ticker] || omx.closing_price(ticker, actual_date)
+      value += quantity * price
+    end
+    value
+  end
+
+  private
+
+  def omx
+    @omx ||= Omx::Source.new
+  end
+
+end
---
+++ spec/portfolio_spec.rb
@@ -0,0 +1,54 @@
+require 'portfolio'
+require 'omx'
+
+describe Portfolio do
+
+  describe "#value" do
+
+    it "fetches the stock prices" do
+      Omx::Source.any_instance.should_receive(:closing_price).with("indu-c", "2011-01-01").and_return(118.2)
+      p = Portfolio.new "2011-01-01", { "indu-c" => 1000 }
+      p.value.should eql 1000 * 118.2
+    end
+
+    it "use price from #initialize instead of fetching if available" do
+      Omx::Source.any_instance.should_not_receive(:closing_price)
+      p = Portfolio.new "2011-01-01", { "indu-c" => 1000 }, 1, { "indu-c" => 99.99 }
+      p.value.should eql 1000 * 99.99
+    end
+
+    it "use explicit price from #value instead of fetching if available" do
+      Omx::Source.any_instance.should_not_receive(:closing_price)
+      p = Portfolio.new "2011-01-01", { "indu-c" => 1000 }
+      p.value(prices: { "indu-c" => 88.88 }).should eql 1000 * 88.88
+    end
+
+    it "fetches the stock prices for an explicit date" do
+      Omx::Source.any_instance.should_receive(:closing_price).with("indu-c", "2011-02-02").and_return(77.77)
+      p = Portfolio.new "2011-01-01", { "indu-c" => 1000 }
+      p.value(date: "2011-02-02").should eql 1000 * 77.77
+    end
+
+  end
+
+  describe "#transaction" do
+    let(:portfolio) { Portfolio.new }
+
+    it "returns a new portfolio" do
+      portfolio.transaction("2001-01-01", "indu-c", 1000, 118.2).should_not eql portfolio
+    end
+
+    it "calculates growth for one stock" do
+      portfolio.transaction("2001-01-01", "indu-c", 1000, 92).
+        transaction("2001-01-02", "indu-c", 1000, 101.2).
+        growth.should eql 1.1
+    end
+
+  end
+
+  describe "#dividend" do
+
+  end
+
+end
+
